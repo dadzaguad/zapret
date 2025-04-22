ae11ecdf30ed39833fa7da987a2730b47fda7f0b
dadza <95094746+dadzaguad@users.noreply.github.com>
2025-04-22 21:08:35 +0300

    minor fixes

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 import sys
-
-# Импорт GUI
-from PyQt6.QtWidgets import QApplication, QMessageBox
+from PyQt6.QtWidgets import QApplication
 from src.gui.gui import CommandRunnerApp
 from src.core.admin_utils import is_admin, restart_as_admin
 
 if __name__ == "__main__":
-    # Проверка запуска с правами администратора
     if not is_admin():
         restart_as_admin()
 
-    # Запуск приложения
     app = QApplication(sys.argv)
     window = CommandRunnerApp()
     window.show()
