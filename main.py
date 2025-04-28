fea803fb1d312223f3e02e4ecc1160b873dc8d50
dadza <95094746+dadzaguad@users.noreply.github.com>
2025-04-28 16:39:58 +0300

    moved the creation of the zapretrunner instance

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 from PyQt6.QtWidgets import QApplication, QMessageBox
 from src.gui.gui import CommandRunnerApp
 from src.core.admin_utils import is_admin, restart_as_admin
+from src.scripts.commands import ZapretRunner
 from src.gui.translations import translator
 
 if __name__ == "__main__":
@@ -12,10 +13,22 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
 
     try:
-        window = CommandRunnerApp()
+        zapret_runner_instance = ZapretRunner()
+
+        window = CommandRunnerApp(zapret_runner=zapret_runner_instance)
         window.show()
+
         sys.exit(app.exec())
+
+    except RuntimeError as e:
+        QMessageBox.critical(
+            None,
+            translator.translate("initialization_error_title", "Initialization Error"),
+            f"{translator.translate('initialization_error_message', 'Failed to initialize the application:')}\n{e}",
+        )
+        sys.exit(1)
     except Exception as e:
+
         QMessageBox.critical(
             None,
             translator.translate("critical_error_title", "Critical Error"),
